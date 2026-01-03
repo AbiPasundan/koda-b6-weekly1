e38359b4a90e65c053559864953957091927c90f
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2026-01-03 14:23:33 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+
+
+
+// const rl = readline.createInterface({ input, output });
+
+// const answer = await rl.question('What do you think of Node.js? ');
+
+// console.log(`Thank you for your valuable feedback: ${answer}`);
+
+// rl.close();
+
+// readline.on('SIGINT', function() {
+//   readline.question('Are you sure you want to exit?', function(answer) {
+//     if (answer.match(/^y(es)?$/i)) readline.pause();
+//   });
+// });
+
+
+
+// const data = {}
+
+// fetch(url)
+//   .then(obj => {
+//     // 1
+//     obj.text()
+//     .then(val => {
+//         const data = JSON.parse(val)
+//         const result = data.map(x => {
+//           console.log(x);
+//         })
+//         console.log(result);
+//     })
+
+//     // 2
+//     // obj.json().then(data => {
+//     //     console.log(data[0].name);
+//     // })
+// })
+// console.log(data);
+
+
+
+
